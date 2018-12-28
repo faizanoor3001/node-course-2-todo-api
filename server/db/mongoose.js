21d63e367ee94dc6895aa7125d635ced4eef3909
faiza.noor <faizanoor3001@gmail.com>
2018-12-28 23:20:09 +0530

    adding mongoose demo with post todos

---
+++ server/db/mongoose.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+const url = 'mongodb://localhost:27017/TodoApp';
+mongoose.connect(url, { useNewUrlParser: true }, (err, client) => {
+  if(err) {
+    return console.log('Unable to connect to MongoDb server');
+  }
+  console.log('Connected to MongoDb server');
+});
+
+module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -1,94 +1,28 @@
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://localhost:27017/TodoApp');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-const url = 'mongodb://localhost:27017/TodoApp';
-mongoose.connect(url, { useNewUrlParser: true }, (err, client) => {
-  if(err) {
-    return console.log('Unable to connect to MongoDb server');
-  }
-  console.log('Connected to MongoDb server');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required : true,
-    minlength : 1,
-    trim : true
+// store express application
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  })
+  todo.save().then((doc) => {
+      res.status(201).send(doc);
   },
-  completed: {
-    type: Boolean,
-    default : false
-  },
-  completedAt : {
-    type: Number,
-    default : null
-  }
+    (e) => {
+        res.status(400).send(e);
+  });
+  //console.log(req.body);
 });
 
-// // testing the validation -->
-// var otherTodo = new Todo({
-//     text : 'Something to do  '
-// });
-// otherTodo.save().then((doc) => {
-//   console.log('saved the other todo');
-// }, (e) => {
-//     console.log('error in saving the todo', e);
-// });
-
-
-
-// var newTodo = new Todo({
-//   text: 'Make Lunch Ready'
-// });
-//
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo--> ', doc);
-// }, (e) => {
-//   console.log('Unable to save todo', e);
-// });
-//
-// var newTodo1 = new Todo({
-//   text: 'Cook Breakfast today',
-//   completed : false,
-//   completedAt : 8
-// });
-//
-// newTodo1.save().then((doc) => {
-//   console.log('Updated a new Todo as well -->', doc);
-// }, (e) => {
-//   console.log('unable to save the todo2');
-// });
-
-
-// // --- user task -->
-// var userSchema = new Schema({
-//   email: {
-//     type: String,
-//     required : true,
-//     trim : true,
-//     minlength : 1
-// });
-//
-// var User = mongoose.model('User', userSchema);
-
-var User = mongoose.model('User' , {
-  email: {
-    type: String,
-    required : true,
-    trim : true,
-    minlength : 1
-  }
-});
-
-var newUser = new User({
-  email: 'faizanoor3001@gmail.com'
-});
-
-newUser.save().then((doc)=> {
-  console.log('created a new user --> ', doc);
-}, (e) => {
-    console.log('Unable to inseert the user => ', e);
-});
-
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
