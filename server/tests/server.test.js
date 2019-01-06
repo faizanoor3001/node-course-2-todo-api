49b2a8b9565aa52f9b86ca7ff674b811c6c6a74f
faiza.noor <faizanoor3001@gmail.com>
2019-01-06 23:36:12 +0530

    adding the test cases for the node todo api

---
+++ server/tests/server.test.js
@@ -0,0 +1,55 @@
+const expect = require('expect');
+const request = require('supertest');
+// const server = require('./server');
+
+// run the tests by commanf  ---> npm run-script test-watch <command>
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+beforeEach((done) => {
+  Todo.remove({}).then(() => done());
+});
+
+describe('POST /todos', () => {
+  it("should create a new todo " , (done) => {
+    var text = 'Test todo text';
+
+    request(app)
+      .post('/todos')
+      .send({text})
+      .expect(201 )
+      .expect((res) => {
+          expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it("should not create a todo with invalid body data", (done) => {
+
+     request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      })
+  });
+});
